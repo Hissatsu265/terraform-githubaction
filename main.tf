0caf7a1556973e6cadd4c31b4139720a3d7aba26
Hissatsu265 <lory265265@gmail.com>
2024-11-13 14:57:53 +0700

    lan 4

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  source             = "./modules/vpc"
-  vpc_cidr_block     = "10.0.0.0/16"
-  public_subnet_cidr = "10.0.1.0/24"
+  source              = "./modules/vpc"
+  vpc_cidr_block      = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = "10.0.2.0/24"
 }
 module "route_tables" {
@@ -15,16 +15,16 @@ module "route_tables" {
 module "security_groups" {
   source = "./modules/security_groups"
   vpc_id = module.vpc.vpc_id
-  my_ip  = "42.114.200.163/32" 
+  my_ip  = "42.114.200.163/32"
 }
 
 module "ec2" {
-  source             = "./modules/ec2"
-  ami_id             = "ami-066a7fbea5161f451"
-  instance_type      = "t2.micro"
-  public_subnet_id   = module.vpc.public_subnet_id
-  private_subnet_id  = module.vpc.private_subnet_id
-  public_sg_id       = module.security_groups.public_sg_id
-  private_sg_id      = module.security_groups.private_sg_id
-  key_pair           = "g16"
+  source            = "./modules/ec2"
+  ami_id            = "ami-066a7fbea5161f451"
+  instance_type     = "t2.micro"
+  public_subnet_id  = module.vpc.public_subnet_id
+  private_subnet_id = module.vpc.private_subnet_id
+  public_sg_id      = module.security_groups.public_sg_id
+  private_sg_id     = module.security_groups.private_sg_id
+  key_pair          = "g16"
 }
